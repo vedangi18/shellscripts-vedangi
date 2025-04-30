c0216009555c69f8787e6cf2af4b64697712bb00
VedangiS <vedangishinde448@gmail.com>
2025-04-30 12:53:43 +0530

    Change-3

--- Script2.sh
+++ Script2.sh
@@ -13,3 +13,6 @@ Class=Young-minds
 Batch=13
 PROFESSION=AWS/DevOps
 echo "Class Name is $Class, Batch number $Batch, We are learning $PROFESSION"
+
+
+echo "Git is a distributed version control system that tracks versions of files. It is often used to control source code by programmers who are developing software collaboratively."
